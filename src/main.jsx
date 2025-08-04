a59f4f730c32ce59dbf7c25456be3e0324412c19
JunYoung Lee <120372886+juneyng@users.noreply.github.com>
2025-08-04 15:30:40 +0900

    feat[#3]: input과 history 페이지 분리

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Analyze from "./pages/Analyze";
+import History from "./pages/History";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Analyze />} />
+      <Route path="/history" element={<History />} />
     </Routes>
   </BrowserRouter>
 );
